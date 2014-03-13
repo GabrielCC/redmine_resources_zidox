844678ec64b28492fb7983a810c854daf1b328fd
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2014-03-13 23:40:29 +0200

    added history integration; fixes #4

--- app/models/issue_resource.rb
+++ app/models/issue_resource.rb
@@ -6,6 +6,10 @@ class IssueResource < ActiveRecord::Base
   validates :estimation, numericality: { only_integer: true }
   validates_uniqueness_of :issue_id, :scope => :resource_id, :message => ' only one estimation for resource'
 
+  after_create 'save_journal(:created)'
+  after_destroy 'save_journal(:deleted)'
+  after_update 'save_journal(:updated)'
+
   def self.from_params(params)
     issue_resource = IssueResource.new
     project = Project.find params[:project_id]
@@ -32,5 +36,21 @@ class IssueResource < ActiveRecord::Base
     hash[:estimation] = self.estimation
     hash[:code] = self.resource.code
     hash
+  end
+
+  private
+
+  def journal_note(operation)
+    messages = {
+      :created => "*Estimation* created #{resource.code} #{estimation}h",
+      :updated => "*Estimation* changed from #{resource.code} #{estimation_was}h to #{resource.code} #{estimation}h",
+      :deleted => "*Estimation* deleted #{resource.code} #{estimation}h"
+    }
+    messages[operation]
+  end
+
+  def save_journal(operation)
+    journal = issue.init_journal(User.current, journal_note(operation))
+    journal.save
   end
 end
