b400cf475e608fc148c5b871ff75a02e14d2a877
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2014-05-30 10:31:53 +0300

    changed hook to put the resource on top of the sidebar

--- lib/hook_listener.rb
+++ lib/hook_listener.rb
@@ -5,6 +5,6 @@ class HookListener < Redmine::Hook::ViewListener
   render_on :view_projects_settings_members_table_row, :partial => 'resources/member'
   render_on :view_projects_settings_add_members, :partial => 'resources/add_membership'
   render_on :view_issues_show_description_bottom, :partial => 'resources/issue'
-  render_on :view_issues_sidebar_queries_bottom, :partial => 'resources/left_column'
+  render_on :view_issues_before_sidebar, :partial => 'resources/left_column'
 
 end
