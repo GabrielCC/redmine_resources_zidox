7984009be8706eb32cd9d04b3cae1104d14c5873
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2014-06-30 09:57:42 +0300

    Update 20140629214789_add_project_resources.rb

--- db/migrate/20140629214789_add_project_resources.rb
+++ db/migrate/20140629214789_add_project_resources.rb
@@ -1,7 +1,7 @@
 class AddProjectResources < ActiveRecord::Migration
   def change
     MemberResource.all.each {|mr|
-      if !mr.member.is_nil? && !mr.resource.is_nil?
+      if !mr.member.nil? && !mr.resource.nil?
         pr = ProjectResource.new
         pr.project = mr.member.project
         pr.resource = mr.resource
