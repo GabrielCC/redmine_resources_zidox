4902f7d418d94b12cee1b9eb7b01044f078a3ba3
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-02-19 12:03:39 +0200

    Refactored patches

--- lib/redmine_resources/patches/project_patch.rb
+++ lib/redmine_resources/patches/project_patch.rb
@@ -1,32 +1,18 @@
 require_dependency 'project'
+
 module RedmineResources
   module Patches
-# Patches Redmine's Project dynamically.  Adds a relationship
-# Project +has_many+ to Resource
-module ProjectPatch
-  def self.included(base) # :nodoc:
-    base.extend(ClassMethods)
-
-    base.send(:include, InstanceMethods)
-
-    # Same as typing in the class
-    base.class_eval do
-      has_many :project_resource, dependent: :destroy
-      has_many :resource, :through => :project_resource
+    module ProjectPatch
+      def self.included(base)
+        base.class_eval do
+          has_many :project_resource, dependent: :destroy
+          has_many :resource, through: :project_resource
+        end
+      end
     end
-
-  end
-
-  module ClassMethods
-  end
-
-  module InstanceMethods
-  end
-end
   end
 end
 
-
-  unless Project.included_modules.include? RedmineResources::Patches::ProjectPatch
-    Project.send(:include, RedmineResources::Patches::ProjectPatch)
-  end
+unless Project.included_modules.include? RedmineResources::Patches::ProjectPatch
+  Project.send :include, RedmineResources::Patches::ProjectPatch
+end
