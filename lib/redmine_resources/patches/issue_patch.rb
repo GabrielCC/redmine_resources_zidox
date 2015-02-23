82df174adfcadbb86e647faf3b33f438282d4087
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-02-23 16:08:25 +0200

    Handeling resource on create

--- lib/redmine_resources/patches/issue_patch.rb
+++ lib/redmine_resources/patches/issue_patch.rb
@@ -8,6 +8,7 @@ module RedmineResources
           has_many :resource, through: :issue_resource
           validate :validate_resources_workflow
           before_save :add_resource_estimation
+          after_save :save_resource_estimation, if: -> { @add_resource_after_save }
         end
       end
 
@@ -43,29 +44,37 @@ module RedmineResources
 
         def add_resource_estimation
           return unless estimated_hours_changed?
-          old_value = estimated_hours_was || 0
-          new_value = estimated_hours || 0
-          difference = (new_value - old_value).floor
-          p "Difference: #{difference}"
-          issue_resource = find_issue_resource
-          estimation = (issue_resource.estimation || 0) + difference
+          old_value = (estimated_hours_was || 0).floor
+          new_value = (estimated_hours || 0).ceil
+          difference = new_value - old_value
+          @altered_resource = find_issue_resource
+          estimation = (@altered_resource.estimation || 0) + difference
           mode = nil
           if estimation < 0
             errors.add :estimation, 'can\'t be decreased that much'
             return false
           elsif estimation == 0
-            unless issue_resource.new_record?
-              issue_resource.destroy
+            unless @altered_resource.new_record?
+              @altered_resource.destroy
               mode = :destroy
             end
           else
-            issue_resource.estimation = estimation
-            mode = issue_resource.new_record? ? :create : :update
-            issue_resource.save
+            @altered_resource.estimation = estimation
+            mode = @altered_resource.new_record? ? :create : :update
+            unless new_record?
+              @altered_resource.save
+            else
+              @add_resource_after_save = true
+            end
           end
-          self.estimated_hours = find_total_estimated_hours
+          self.estimated_hours = new_record? ? estimation : find_total_estimated_hours
           return unless @current_journal && mode
-          @current_journal.details << issue_resource.journal_entry(mode, old_value)
+          @current_journal.details << @altered_resource.journal_entry(mode, old_value)
+        end
+
+        def save_resource_estimation
+          @altered_resource.issue_id = id
+          @altered_resource.save
         end
 
         def find_issue_resource
