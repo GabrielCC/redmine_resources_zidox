855fba2cb12246018e15a92aef0dfd5256c460d7
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-04-10 11:59:12 +0300

    Added debug logging and it's here to stay

--- lib/redmine_resources/patches/issue_patch.rb
+++ lib/redmine_resources/patches/issue_patch.rb
@@ -29,32 +29,40 @@ module RedmineResources
         end
 
         def track_estimation_change
+          logger.debug  "---track_estimation_change"
           if parent_issue_id != parent_id
+            logger.debug  "Parent changed!"
             @parent_changed = true
             @old_parent_id = parent_id
+            logger.debug  "Old Parent ID: #{@old_parent_id}"
             @new_parent_id = parent_issue_id
+            logger.debug  "New Parent ID: #{@new_parent_id}"
           else
+            logger.debug  "Parent not changed!"
             @old_parent_id = parent_issue_id
           end
           @old_estimation = estimated_hours_was.to_i
+          logger.debug  "Old estimation: #{@old_estimation}"
           @new_estimation = estimated_hours
+          logger.debug  "New estimation: #{@new_estimation}"
         end
 
         def calculate_resource_estimation_for_self
+          logger.debug  "---calculate_resource_estimation_for_self"
           estimation = @estimation_value.to_i
+          logger.debug  "estimation: #{estimation}"
           return true if estimation == 0
-          resource_id = determine_resource_type_id
-          @altered_resource = find_issue_resource id
-          @altered_resource.estimation = estimation
-          mode = @altered_resource.new_record? ? :create : :update
-          @altered_resource.save
-          return true unless @current_journal
-          @current_journal.details << @altered_resource.journal_entry(mode, @old_estimation)
+          altered_resource = ensure_current_issue_resource_for id
+          logger.debug  "altered_resource #{altered_resource}"
+          altered_resource.estimation = estimation
+          altered_resource.save
         end
 
         def recalculate_resources_for(parent_id)
+          logger.debug  "---recalculate_resources_for #{parent_id}"
           parent_issue = Issue.where(id: parent_id).first || return
           return unless parent_gets_resources?(parent_issue)
+          logger.debug  "Parent found"
           ensure_current_issue_resource_for parent_issue
           parent_issue.estimated_hours = 0
           issue_resource.all.each do |res|
@@ -64,55 +72,72 @@ module RedmineResources
         end
 
         def calculate_resource_estimation_for_parent
+          logger.debug  "---calculate_resource_estimation_for_parent"
           recalculate_resources_for @old_parent_id if @old_parent_id
           recalculate_resources_for @new_parent_id if @new_parent_id
         end
 
-        def ensure_current_issue_resource_for(parent_id)
-          IssueResource.where(issue_id: parent_id,
+        def ensure_current_issue_resource_for(issue_id)
+          logger.debug  "---ensure_current_issue_resource_for"
+          IssueResource.where(issue_id: issue_id,
             resource_id: determine_resource_type_id
           ).first_or_create(estimation: 1)
         end
 
         def recalculate_from_children(res, parent_issue)
+          logger.debug  "---recalculate_from_children #{res.inspect}, #{parent_issue.inspect}"
           issues = Issue.where(parent_id: parent_issue.id)
             .where('issues.status_id NOT IN (?)', [6,23])
             .select(:estimated_hours, :assigned_to_id, :author_id)
           estimated = 0
           issues.each do |issue|
             member = Member.where(user_id: (issue.assigned_to_id || issue.author_id), project_id: project_id).first
+            logger.debug  "Member: #{member.inspect}"
             next unless member
             member_resource = member.resource
             next unless member_resource
             estimated += issue.estimated_hours if member_resource.id == res.resource_id
           end
           res.estimated = estimated.to_i
+          logger.debug  "Res: #{res.inspect}"
           res.estimated == 0 ? res.destroy : res.save
         end
 
         def determine_resource_type_id
+          logger.debug  "---determine_resource_type_id"
           user_id = assigned_to_id || author_id
+          logger.debug  "user id: #{user_id}"
           member = Member.where(user_id: user_id, project_id: project_id).first
+          logger.debug  "member: #{member}"
           return nil unless member
           member_resource = member.resource
+          logger.debug  "member resource: #{member_resource.inspecr}"
           member_resource ? member_resource.id : nil
         end
 
         def update_estimation_for(parent_issue)
-          return if parent.blocked?
+          logger.debug  "---update_estimation_for #{parent_issue.inspect}"
+          logger.debug  "Blocked? #{parent_issue.blocked?}"
+          return if parent_issue.blocked?
           children_estimation_total = Issue.where(
               'issues.tracker_id NOT IN (2,5,6) AND parent_id = ?', parent_issue.id
             ).sum(:estimated_hours).to_i
           children_estimation_total += Issue.where(parent_id: @parent_id, tracker_id: 2)
             .sum(:estimated_hours).to_i if parent.tracker_id == 5
+          logger.debug  "children_estimation_total: #{children_estimation_total}"
           parent.update_column :estimated_hours, children_estimation_total
           parent.update_parent_estimation
+          logger.debug  "Parent column updated"
         end
 
         def parent_gets_resources?(parent_issue)
+          logger.debug  "---parent_gets_resources? #{parent_issue.inspect}"
+          logger.debug  "Manual estimation: #{parent_issue.manually_added_resource_estimation}"
           return false if parent_issue.manually_added_resource_estimation
           trackers_with_resources = ResourceSetting.where(project_id: project_id,
             setting: 1, setting_object_type: 'Tracker').pluck(:setting_object_id)
+          logger.debug  "Trackers with resources: #{trackers_with_resources}"
+          logger.debug  "Parent trackerid: #{parent_issue.tracker_id}"
           !trackers_with_resources.include?(parent_issue.tracker_id) &&
             ![2,5,6].include?(parent_issue.tracker_id)
         end
