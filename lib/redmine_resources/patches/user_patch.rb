c7fda9b3e2784da1d578b72ac3f0564f0c9f3bf6
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-06-17 19:03:51 +0300

    Added a patch to the user model

---
+++ lib/redmine_resources/patches/user_patch.rb
@@ -0,0 +1,32 @@
+module RedmineResources
+  module Patches
+    module UserPatch
+      def self.included(base)
+        base.send :include, InstanceMethods
+      end
+
+      module InstanceMethods
+        def resource_names
+          @resource_names ||= resources.map do |resource|
+              "#{ resource.name } #{ resource.code }"
+            end.join(', ')
+        end
+
+        def resources
+          Resource.joins('INNER JOIN project_resources ON project_resources.resource_id = resources.id')
+            .joins('INNER JOIN projects ON projects.id = project_resources.project_id')
+            .joins('INNER JOIN project_resource_emails ON project_resource_emails.project_id = projects.id AND project_resource_emails.resource_id = resources.id')
+            .joins('INNER JOIN members ON members.project_id = projects.id')
+            .joins('INNER JOIN users ON users.id = members.user_id ')
+            .where('users.id = ?', id)
+            .where('project_resource_emails.email = ?', mail)
+            .select('DISTINCT resources.name, resources.code')
+        end
+      end
+    end
+  end
+end
+
+unless User.included_modules.include? RedmineResources::Patches::UserPatch
+  User.send :include, RedmineResources::Patches::UserPatch
+end
